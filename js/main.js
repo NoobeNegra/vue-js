6ab7851ca5214eb29959538bd7f4bbce0452ba2c
NoobeNegra <morenoailin@gmail.com>
2020-05-09 22:15:27 +0200

    Tool List + Tool Detail + User Detail

--- js/main.js
+++ js/main.js
@@ -1,43 +1,98 @@
-var divProduct = new Vue({
-    el: '#divProduct',
-    data: {
-        product: 'Socks',
-        description: 'This socks will not suck',
-        selectedColor: 0,
-        details: ["100% coton", "You will not sweat", "Excellent for ski"],
-        sizes: ["30-34", "35-37", "38-41", "42-45"],
-        colors: [{
-            name: "#405184",
-            image: "./img/vmSocks-blue-onWhite.jpg",
-            alt: 'A nice pair of blue socks',
-            stock: 20
-        }, {
-            name: "#199E4F",
-            image: "./img/vmSocks-green-onWhite.jpg",
-            alt: 'A nice pair of green socks',
-            stock: 10
-        }
-        ],
-        cart: 0,
+Vue.component('toolDescriptionComponent', {
+    template: '#tool-description-template',
+    data() {
+        return { tool: null, hide: true }
     },
     methods: {
-        addToCart: function () {
-            this.cart += 1;
-            this.colors[this.selectedColor].stock--;
+        showInfo: function (toolId) {
+            this.hide = false;
+            axios
+                .get('http://localhost:8000/api/tool/' + toolId)
+                .then(response => (this.tool = response.data))
         },
-        removeFromCart: function () {
-            if (this.cart > 0) {
-                this.cart -= 1;
-                this.colors[this.selectedColor].stock++;
-            }
+        askToBorrow: function () {
+            console.log('ok');
         },
-        changeImage: function (index) {
-            this.selectedColor = index;
+        back: function () {
+            this.hide = true;
+            this.$emit('showtoollist');
+        },
+        showOwner: function (ownerId) {
+            this.hide = true;
+            this.$emit('showuserinfo', ownerId);
         }
     },
     computed: {
-        image() { return this.colors[this.selectedColor].image },
-        imageDescription() { return this.colors[this.selectedColor].alt },
-        inventory() { return this.colors[this.selectedColor].stock }
+        owner() { return this.tool.user.name + ' ' + this.tool.user.surename },
+        ownerid() { return this.tool.user.id },
+        exchanges() { return this.tool.exchanges },
+        comments() { return this.tool.comments }
+    },
+});
+
+Vue.component('userInformationTemplate', {
+    template: '#user-information-template',
+    data() {
+        return {
+            user: null,
+            hide: true,
+            toolid: null
+        }
+    },
+    methods: {
+        showInfo: function (userId, toolId) {
+            this.hide = false;
+            this.toolid = toolId;
+            axios
+                .get('http://localhost:8000/api/user/' + userId)
+                .then(response => (this.user = response.data))
+        },
+        back: function () {
+            this.hide = true;
+            this.$emit('showtoolinformation', this.toolid);
+        }
+    },
+    computed: {
+        name() { return this.user.name + ' ' + this.user.surename },
+        comments() { return this.user.comments }
+    },
+});
+
+Vue.component('toolListComponent', {
+    template: '#tool-list-template',
+    data() {
+        return {
+            listTools: null,
+            shouldHide: false
+        }
+    },
+    methods: {
+        showTool: function (toolId) {
+            this.shouldHide = true;
+            this.$emit('showtoolinformation', toolId);
+        },
+        show: function () {
+            this.shouldHide = false;
+        }
+    },
+    mounted() {
+        axios
+            .get('http://localhost:8000/api/tools')
+            .then(response => (this.listTools = response.data))
     }
+});
+
+new Vue({
+    el: '#container',
+    methods: {
+        showtoolinformation: function (toolId) {
+            this.$refs.descriptioncomponent.showInfo(toolId);
+        },
+        showtoollist: function () {
+            this.$refs.toollistcomponent.show();
+        },
+        showuserinfo: function (ownerId) {
+            this.$refs.userinfocomponent.showInfo(ownerId);
+        }
+    },
 });
